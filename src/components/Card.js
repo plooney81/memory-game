14b0f990f0c04e890f0a24bebbdab3847d70ab2f
Pete Looney <petelooney81@gmail.com>
2020-10-26 10:48:52 -0500

    event handler for card click added

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,29 @@ import './Card.css'
 import logo from './logo-wrench-white.png'
 
 class Card extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isFlipped: false
+        };
+    }
+
+    clickHandler = () => {
+        this.setState({
+            isFlipped: !this.state.isFlipped
+        })
+    }
+
     render(){
+        let innerClass = "Card_inner";
+        if(this.state.isFlipped){
+            innerClass += ' flipped';
+        }
         return(
-            <div className="Card">
-                <div className="Card_inner">
+            <div className="Card" onClick={this.clickHandler}>
+                <div className={innerClass}>
                     <div className="Card_front">
                         ∆
                     </div>
