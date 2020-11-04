c0963dba45207030f8b52873e9ace64064b2797b
Pete Looney <petelooney81@gmail.com>
2020-11-04 11:21:40 -0600

    Refactored for hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card'
 
-const deck = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+const blankDeck = ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
 
 const generateDeck = () => {
   const symbols = ['∆', 'ß', '£', '§', '•', '$', '+', 'ø'];
   const emptyDeck = [];
-  deck.forEach((slot, index)=>{
+  blankDeck.forEach((slot, index)=>{
     emptyDeck.push({isFlipped: false, symbol: symbols[index%8]})
   })
 
@@ -20,33 +20,40 @@ const generateDeck = () => {
   return emptyDeck;
 }
 
-class App extends React.Component{
+function App() {
+  // this.state = {
+  //   deck: generateDeck(),
+  //   pickedCards: [],
+  //   displayButton: false,
+  //   matchedCards: 0
+  // };
+    const [ deck, setDeck ] = useState(generateDeck());
+    const [ pickedCards, setPickedCards ] = useState([])
+    const [displayButton, setDisplayButton] = useState(false);
+    const [matchedCards, setMatchedCards ] = useState(0);
 
-  constructor(props){
-      super(props);
 
-      this.state = {
-        deck: generateDeck(),
-        pickedCards: [],
-        displayButton: false,
-        matchedCards: 0
-      };
+   
+  
+
+  const resetGame = () => {
+    // this.setState({
+    //   deck: generateDeck(),
+    //   pickedCards: [],
+    //   displayButton: false,
+    //   matchedCards: 0
+    // })
+    setDeck(generateDeck());
+    setPickedCards([]);
+    setDisplayButton(false);
+    setMatchedCards(0);
   }
 
-  resetGame = () => {
-    this.setState({
-      deck: generateDeck(),
-      pickedCards: [],
-      displayButton: false,
-      matchedCards: 0
-    })
-  }
-
-  unflipCards = (card1Index, card2Index) => {
-    const [card1, card2] = [{...this.state.deck[card1Index]}, {...this.state.deck[card2Index]}]
+  const unflipCards = (card1Index, card2Index) => {
+    const [card1, card2] = [{...deck[card1Index]}, {...deck[card2Index]}]
     card1.isFlipped = false;
     card2.isFlipped = false;
-    const newDeck = this.state.deck.map((card, index)=>{
+    const newDeck = deck.map((card, index)=>{
       if(card1Index === index){
         return card1
       }else if(card2Index === index){
@@ -54,23 +61,23 @@ class App extends React.Component{
       }
       return card;
     })
-    this.setState({ deck: newDeck })
+    setDeck(newDeck);
   }
   
   // returns nothing if the card is already flipped
-  pickCard = (cardIndex) => {
-    if(this.state.deck[cardIndex].isFlipped){
+  const pickCard = (cardIndex) => {
+    if(deck[cardIndex].isFlipped){
       return '';
     }
     // React won't let us mutate state directly, we have to use setState(), but since
     // our state has two arrays we want to push to, we unforutnately can do this.state.array.push(whatever)
     // we instaed have to create copies and use them to make our changes
-    const cardToFlip = {...this.state.deck[cardIndex]};
+    const cardToFlip = {...deck[cardIndex]};
     cardToFlip.isFlipped = true;
     // array.prototype.concat creates a copy and inserts that value in paren.
-    let newPickedCards = this.state.pickedCards.concat(cardIndex);
-    let matchedCards = this.state.matchedCards;
-    const newDeck = this.state.deck.map((card, index)=>{
+    let newPickedCards = pickedCards.concat(cardIndex);
+    let newMatchedCards = matchedCards;
+    const newDeck = deck.map((card, index)=>{
       if(cardIndex === index){
         return cardToFlip
       }
@@ -80,39 +87,41 @@ class App extends React.Component{
 
     if(newPickedCards.length === 2){
       const [card1Index, card2Index] = [newPickedCards[0], newPickedCards[1]];
-      if(newDeck[card1Index].symbol !== newDeck[card2Index].symbol){ setTimeout(()=>{this.unflipCards(card1Index, card2Index)}, 1000) }
-      if(newDeck[card1Index].symbol === newDeck[card2Index].symbol){matchedCards += 2}
+      if(newDeck[card1Index].symbol !== newDeck[card2Index].symbol){ setTimeout(()=>{unflipCards(card1Index, card2Index)}, 1000) }
+      if(newDeck[card1Index].symbol === newDeck[card2Index].symbol){newMatchedCards += 2}
       newPickedCards = [];
     }
     
-    this.setState({
-      deck: newDeck,
-      pickedCards: newPickedCards,
-      matchedCards: matchedCards,
-      displayButton: matchedCards === 16
-    })
+    // this.setState({
+    //   deck: newDeck,
+    //   pickedCards: newPickedCards,
+    //   matchedCards: matchedCards,
+    //   displayButton: matchedCards === 16
+    // })
+    setDeck(newDeck);
+    setPickedCards(newPickedCards);
+    setMatchedCards(newMatchedCards);
+    setDisplayButton(matchedCards === 16);
   }
 
-  render(){
     return (
       <div className="App">
         <div className="App-header">
           <h1>Memory Game</h1>
           
-          {this.state.displayButton ? <h2>Congrats you won!!</h2> : <h2>Match Cards to win</h2>}
+          {displayButton ? <h2>Congrats you won!!</h2> : <h2>Match Cards to win</h2>}
         </div>
         <div className="body">
         <div className="container">
-          {this.state.deck.map((card, index)=>{
+          {deck.map((card, index)=>{
             // How to send props
-            return <Card key={index} symbol={card.symbol} isFlipped={card.isFlipped} pickCard={this.pickCard.bind(this, index)}/>
+            return <Card key={index} symbol={card.symbol} isFlipped={card.isFlipped} pickCard={pickCard.bind(this, index)}/>
           })}
         </div>
         </div>
-        {this.state.displayButton ? <button onClick={this.resetGame}>Reset</button> : ''}
+        {displayButton ? <button onClick={resetGame}>Reset</button> : ''}
       </div>
     );
-  }
 
 }
 export default App;
